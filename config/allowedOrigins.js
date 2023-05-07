56a4665c0af28215186a821a760f6724683365f3
VelmaGotWaves <ognjen.mobilni2407@gmial.com>
2023-05-07 23:23:53 +0200

    asdas

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'http://localhost:3500',
     'http://localhost:3000',
     'http://127.0.0.1:5173',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://baza-nastavnika-react.onrender.com'
 ];
 
 module.exports = allowedOrigins;
